46c278fd1b5aac8f04a6facf1dfd4426ddbe3d51
Nick Oppen <nick@oppen.net.au>
2014-09-05 11:52:11 +0000

    Change-Id: I444a7110c19d702200e43d4169d92da23c16f98b

---
+++ Debug/src/nD.d
@@ -0,0 +1,45 @@
+src/nD.d: ../src/nD.cpp /usr/local/browndeer/include/stdcl.h \
+ /usr/local/browndeer/include/stdcl_host.h \
+ /usr/local/browndeer/include/CL/cl.h \
+ /usr/local/browndeer/include/CL/cl_platform.h \
+ /usr/local/browndeer/include/CL/cl_gl.h \
+ /usr/local/browndeer/include/clcontext.h \
+ /usr/local/browndeer/include/clinit.h \
+ /usr/local/browndeer/include/clelf.h \
+ /usr/local/browndeer/include/clsched.h \
+ /usr/local/browndeer/include/clmalloc.h \
+ /usr/local/browndeer/include/clfcn.h \
+ /usr/local/browndeer/include/clarg.h \
+ /usr/local/browndeer/include/clerror.h \
+ /usr/local/browndeer/include/clforka.h \
+ /usr/local/browndeer/include/clexec.h
+
+/usr/local/browndeer/include/stdcl.h:
+
+/usr/local/browndeer/include/stdcl_host.h:
+
+/usr/local/browndeer/include/CL/cl.h:
+
+/usr/local/browndeer/include/CL/cl_platform.h:
+
+/usr/local/browndeer/include/CL/cl_gl.h:
+
+/usr/local/browndeer/include/clcontext.h:
+
+/usr/local/browndeer/include/clinit.h:
+
+/usr/local/browndeer/include/clelf.h:
+
+/usr/local/browndeer/include/clsched.h:
+
+/usr/local/browndeer/include/clmalloc.h:
+
+/usr/local/browndeer/include/clfcn.h:
+
+/usr/local/browndeer/include/clarg.h:
+
+/usr/local/browndeer/include/clerror.h:
+
+/usr/local/browndeer/include/clforka.h:
+
+/usr/local/browndeer/include/clexec.h:
